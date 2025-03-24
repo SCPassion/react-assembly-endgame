06b291ca54ca3fe44b3740d983525b228cfad958
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-03-24 21:01:44 +0100

    Build basic page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
 import { languages } from "./languages.js";
 import { nanoid } from "nanoid";
+import React from "react";
 
 export default function () {
+  const [hiddenWord, setHiddenWord] = React.useState("react");
+
+  const allLetters = "abcdefghijklmnopqrstuvwxyz";
+  const languageElements = languages.map((language) => {
+    const style = {
+      backgroundColor: language.backgroundColor,
+      color: language.color,
+    };
+
+    return (
+      <span
+        key={nanoid()}
+        style={style}
+        className="flex h-[24.75px] items-center rounded-sm px-[8px] text-[12px] font-bold"
+      >
+        {language.name}
+      </span>
+    );
+  });
+
+  const hiddenWordElements = [...hiddenWord].map((letter) => (
+    <div
+      key={nanoid()}
+      className="border[#F9F4DA] flex h-[40px] w-[40px] items-center justify-center border-b-2 bg-[#323232] text-[#F9F4DA]"
+    >
+      {letter.toUpperCase()}
+    </div>
+  ));
+
+  const allLetterElements = [...allLetters].map((letter) => {
+    const style = { backgroundColor: "#FCBA29" };
+
+    return (
+      <button
+        style={style}
+        className="h-[40px] w-[40px] cursor-pointer rounded-[4px] border-[1px] border-[#D7D7D7]"
+        key={nanoid()}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
   return (
     <>
       <header className="w-[352px] text-center">
@@ -14,12 +58,28 @@ export default function () {
         </p>
       </header>
 
-      <main>
-        <section className="flex h-[59px] w-[352px] flex-col items-center justify-center rounded-md bg-[#10A95B] text-center text-[#F9F4DA]">
+      <main className="flex flex-col items-center gap-[16px]">
+        <section className="py-auto flex h-[59px] w-[352px] flex-col items-center justify-center rounded-md bg-[#10A95B] text-[#F9F4DA]">
           <h2>You win!</h2>
           <p>Well done! 🎉</p>
         </section>
+
+        <section className="flex w-[300px] flex-wrap justify-center gap-[4px]">
+          {languageElements}
+        </section>
+
+        <section className="flex gap-1">{hiddenWordElements}</section>
       </main>
+
+      <footer className="flex w-[460px] flex-col items-center gap-[16px]">
+        <section className="flex flex-wrap justify-center gap-[6px]">
+          {allLetterElements}
+        </section>
+
+        <button className="flex h-[40px] w-[228px] cursor-pointer items-center justify-center rounded-[4px] border-[1px] border-[#D7D7D7] bg-[#11B5E5]">
+          New Game
+        </button>
+      </footer>
     </>
   );
 }
